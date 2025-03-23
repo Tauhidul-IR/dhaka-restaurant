7eca46b27526a513b8ef76df3b6ff823c74f5329
Robin <robin13690@gmail.com>
2025-03-23 08:31:34 +0600

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
 <button className="btn btn-secondary">Secondary</button>
 <button className="btn btn-accent">Accent</button>
 <button className="btn btn-ghost">Ghost</button>
+<button className="btn btn-ghost">Ghost</button>
 <button className="btn btn-link">Link</button>
     </>
   )

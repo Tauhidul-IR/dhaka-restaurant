72cfd31e91d3fc81e247ddc95685dd4609b7b9d1
Robin <robin13690@gmail.com>
2025-04-11 22:34:44 +0600

    helmat

---
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,29 @@
+import { Helmet } from "react-helmet-async";
+// import Cover from "../../Shared/Cover/Cover";
+// import menuImg from "../../../assets/menu/menu-bg.jpg";
+// import soupImg from "../../../assets/menu/soup-bg.jpg";
+// import saladImg from "../../../assets/menu/salad-bg.jpg";
+// import pizzaImg from "../../../assets/menu/pizza-bg.jpg";
+// import dessertImg from "../../../assets/menu/dessert-bg.jpeg";
+// import useMenu from "../../../hooks/useMenu";
+// import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+// import MenuCategory from "../MenuCategory/MenuCategory";
+
+const Menu = () => {
+//   const [menu] = useMenu();
+//   const desserts = menu.filter((item) => item.category === "dessert");
+//   const soup = menu.filter((item) => item.category === "soup");
+//   const salad = menu.filter((item) => item.category === "salad");
+//   const pizza = menu.filter((item) => item.category === "pizza");
+//   const offered = menu.filter((item) => item.category === "offered");
+  return (
+    <div>
+      <h1>menu</h1>
+      <Helmet>
+        <title>Dhaka Restaurant | Menu</title>
+      </Helmet>
+    </div>
+  );
+};
+
+export default Menu;

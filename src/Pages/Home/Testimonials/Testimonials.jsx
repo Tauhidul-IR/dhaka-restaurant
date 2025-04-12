5b9777c41cbd2053265708ea118e9c8dedd594d9
Robin <robin13690@gmail.com>
2025-04-12 15:01:59 +0600

    server link add

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -20,7 +20,7 @@ const Testimonials = () => {
 //   }, []);
 
   useEffect(() => {
-    fetch('review.json')
+    fetch('http://localhost:5000/reviews')
       .then((res) => res.json())
       .then((data) => setReviews(data));
   }, []);

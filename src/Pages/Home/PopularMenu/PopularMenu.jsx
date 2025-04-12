42d0335b789dec8068fe93618daf1f976a4e063a
Robin <robin13690@gmail.com>
2025-04-12 07:06:01 +0600

    menu

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -2,21 +2,22 @@
 
 import { Link } from "react-router-dom";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-//   const [menu] = useMenu();
-//   const popular = menu.filter((item) => item.category === "popular");
-const [menu, setMenu]=useState([]);
-useEffect(()=>{
-    fetch('menu.json')
-    .then(res=>res.json())
-    .then(data=>{
-        const popularItems = data.filter(item => item.category === 'popular');
-        setMenu(popularItems)
-    })
-},[])
+  const [menu] = useMenu();
+  const popular = menu.filter((item) => item.category === "popular");
+// const [menu, setMenu]=useState([]);
+// useEffect(()=>{
+//     fetch('menu.json')
+//     .then(res=>res.json())
+//     .then(data=>{
+//         const popularItems = data.filter(item => item.category === 'popular');
+//         setMenu(popularItems)
+//     })
+// },[])
 
   return (
     <section className="mb-12">
@@ -25,7 +26,7 @@ useEffect(()=>{
         subHeading="Popular Items"
       ></SectionTitle>
       <div className="grid md:grid-cols-2 gap-10">
-        {menu.map((item) => (
+        {popular.map((item) => (
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
       </div>

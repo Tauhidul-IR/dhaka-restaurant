299283661ec4e16a51f45f8ca683d834f96b9ea6
Robin <robin13690@gmail.com>
2025-04-27 22:10:08 +0600

    tanstack query

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -2,10 +2,11 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
 import { FaShoppingCart } from "react-icons/fa";
+import useCart from "../../../hooks/useCart";
 
   const Navbar = () => {
     const { user, logOut } = useContext(AuthContext);
-
+    const [cart] = useCart();
 
 
     const handleLogOut = () => {
@@ -35,7 +36,7 @@ import { FaShoppingCart } from "react-icons/fa";
         <Link to={"/dashboard/cart"}>
           <button className="btn btn-xs">
             <FaShoppingCart className="mr-1"></FaShoppingCart>
-            <div className="badge badge-sm badge-secondary">+0</div>
+            <div className="badge badge-sm badge-secondary">+{cart.length}</div>
           </button>
         </Link>
       </li>

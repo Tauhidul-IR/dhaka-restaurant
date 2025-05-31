37582c223dfef3b3ee8f85f8a2121e4bbd2f10a7
Robin <robin13690@gmail.com>
2025-05-31 22:31:28 +0600

    menu items

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -61,7 +61,7 @@ const FoodCard = ({ item }) => {
         <img className="w-96 h-72" src={image} alt="Shoes" />
       </figure>
       <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">
-        ${price}
+        Taka:{price}
       </p>
       <div className="card-body flex flex-col items-center">
         <h2 className="card-title">{name}</h2>
